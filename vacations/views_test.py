6b57548588b890f77d71ea73955667d792a0a79e
Leop0ld <l3opold7@gmail.com>
2017-11-07 19:03:53 +0900

    Fix CI :hammer:

---
+++ vacations/views_test.py
@@ -0,0 +1,12 @@
+from django.test import TestCase, Client
+
+
+class VacationViewTest(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_hello(self):
+        assert True is True
