6495a9b71d26bbe9e9ad1ffeaee3c8b3d618eae7
Leop0ld <l3opold7@gmail.com>
2017-11-07 10:11:59 +0900

    Settings 분리 완료

--- vacation_management/wsgi.py
+++ vacation_management/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vacation.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vacation_management.settings.production")
 
 application = get_wsgi_application()
