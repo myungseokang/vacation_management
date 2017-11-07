df87f03114b77d94dc9efbdb729f770379f7b9ba
Leop0ld <l3opold7@gmail.com>
2017-11-07 18:57:58 +0900

    requirements 분리 및 pytest 추가 :fire:

--- vacation_management/settings/local.py
+++ vacation_management/settings/local.py
@@ -2,16 +2,6 @@ from .base import *  # NOQA
 
 INSTALLED_APPS += ('debug_toolbar',)
 
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 
 INTERNAL_IPS = '127.0.0.1'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
