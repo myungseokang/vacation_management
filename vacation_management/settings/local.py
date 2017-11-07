c9472185982f0995f3c74d02d543ed993de637ee
Leop0ld <l3opold7@gmail.com>
2017-11-07 18:01:14 +0900

    Docker 적용~! :whale:

--- vacation_management/settings/local.py
+++ vacation_management/settings/local.py
@@ -5,3 +5,13 @@ INSTALLED_APPS += ('debug_toolbar',)
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
 
 INTERNAL_IPS = '127.0.0.1'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
