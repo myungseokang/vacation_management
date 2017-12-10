d14565ebe2fd6c7a81ae39a20dc07d60b97d0b35
Leop0ld <l3opold7@gmail.com>
2017-12-10 19:48:58 +0900

    CI & Production 세팅

--- vacation_management/settings/production.py
+++ vacation_management/settings/production.py
@@ -2,6 +2,8 @@ import dj_database_url
 
 from .base import *  # NOQA
 
+DEBUG = False
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config()
