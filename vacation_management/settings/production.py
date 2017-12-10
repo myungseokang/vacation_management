7723677ad0e2a2cab1d24aaab3aad712463970fe
Leop0ld <l3opold7@gmail.com>
2017-12-10 19:33:16 +0900

    DATABASE 설정 변경

--- vacation_management/settings/production.py
+++ vacation_management/settings/production.py
@@ -4,5 +4,12 @@ from .base import *  # NOQA
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
